8664df3683063372b23d4f492888588ac2e92951
DepressedTuring <shayan.atashbarazar@gmail.com>
2025-09-13 10:08:05 +0330

    settings for wsgi fixed

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-4()pys&yu+5(1b=qr(auxu)udmc(_!59rw$l@e4y2a$jdbf_c7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["pasaan.ir",]
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
